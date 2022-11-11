1c537c64c25abc36229572b45263054b58fe45fe
StephenJohnson <sjjair@yahoo.com>
2022-11-10 17:19:58 -0800

    Added image animation

--- Big Robot Micro Client Services/MainContentViews/MainPage.swift
+++ Big Robot Micro Client Services/MainContentViews/MainPage.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct MainPage: View {
+    @State var bobbing: Bool = false
     var body: some View {
         NavigationView {
             VStack {
@@ -16,6 +17,22 @@ struct MainPage: View {
                     .fontWeight(.bold)
                     .padding(EdgeInsets(top: 30, leading: 0, bottom: 10, trailing: 0))
                              
+                Spacer()
+                
+                Image("desk")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(20)
+                    .shadow(radius: 15)
+                    .padding(33)
+                    .offset(y: bobbing ? 4 : 0)
+                    .onAppear(perform: {
+                        withAnimation(Animation.easeInOut(duration: 1.0).repeatForever()){
+                            self.bobbing.toggle()
+                        }
+                    })
+                    
+                
                 Spacer(minLength: 100)
                     List {
                         NavigationLink(destination: SettingsPage()) {
