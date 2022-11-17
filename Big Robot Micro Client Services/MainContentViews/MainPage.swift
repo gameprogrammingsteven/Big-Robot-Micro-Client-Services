c7e891a975dd17166b99db71170c35f4c4f8098e
StephenJohnson <sjjair@yahoo.com>
2022-11-17 12:55:32 -0800

    Main page tidied
    Weather tidied

--- Big Robot Micro Client Services/MainContentViews/MainPage.swift
+++ Big Robot Micro Client Services/MainContentViews/MainPage.swift
@@ -10,62 +10,118 @@ import ARKit
 
 struct MainPage: View {
     @State var bobbing: Bool = false
+    
+    let mainURL = URL(string:"https://bigrobotmicro.com")!
+    
+    @AppStorage("onboarding")
+    private var showOnboarding: Bool = false
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Samples")
-                    .font(.custom("Arial", size: 36.0))
-                    .fontWeight(.bold)
-                    .padding(EdgeInsets(top: 30, leading: 0, bottom: 10, trailing: 0))
-                             
-                Spacer()
-                
-                Image("desk")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(20)
-                    .shadow(radius: 15)
-                    .padding(33)
-                    .offset(y: bobbing ? 4 : 0)
-                    .onAppear(perform: {
-                        withAnimation(Animation.easeInOut(duration: 1.0).repeatForever()){
-                            self.bobbing.toggle()
-                        }
-                    })
+            ScrollView {
+                VStack {
+                    Text("Samples")
+                        .foregroundColor(.white)
+                        .font(.custom("Arial", size: 36.0))
+                        .fontWeight(.bold)
+                        .shadow(color: .black, radius: 3, x: 4, y: 2)
+                        .padding(EdgeInsets(top: 30, leading: 0, bottom: 10, trailing: 0))
                     
-                
-                Spacer(minLength: 100)
-                    List {
-//                        NavigationLink(destination: SettingsPage()) {
-//                            Text("Set")
-//                        }
-                        NavigationLink(destination: BLEPage()) {
-                            Text("Bluetooth")
-                        }
-                        NavigationLink(destination: GraphDemosView()) {
-                            Text("Graphs")
-                        }
-                        NavigationLink(destination: WeatherMockView()) {
-                            Text("Fake Weather Demo Page")
-                        }
-//                        if (ARConfiguration.isSupported) {
-//                            NavigationLink(destination: ART()) {
-//                                Text("Fake ARTest")
+                    Spacer(minLength: 50)
+                    
+                    Image("desk")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(20)
+                        .shadow(radius: 15)
+                        .padding(33)
+                        .offset(y: bobbing ? 4 : 0)
+                        .animation(Animation.easeInOut(duration:3.0), value: bobbing)
+//                        .onAppear() {
+//                            withAnimation(Animation.easeInOut(duration: 3.0).repeatForever()){
+//                                self.bobbing.toggle()
 //                            }
 //                        }
-
+                    
+                    
+                    Spacer(minLength: 50)
+                    
+                    ScrollView {
+                        NavigationLink(destination: WeatherMockView()) {
+                            HStack(alignment: .center) {
+                                
+                                
+                                Text("Sample Chart")
+                                    .foregroundColor(Color.black)
+                                    .fontWeight(.heavy)
+                                    .padding()
+                                Spacer()
+                                Image(systemName:"chevron.compact.right").foregroundColor(.white).scaleEffect(1.6)
+                                    .padding()
+                            }
+                        }
+                        .frame(minHeight: 32)
+                        HStack(alignment: .center) {
+                            Text("Website")
+                                .fontWeight(.heavy)
+                                .padding()
+                            Spacer()
+                            Image(systemName:"chevron.compact.right").padding().foregroundColor(.white).scaleEffect(1.6)
+                            
+                        }
+                        .onTapGesture {
+                            print("t")
+                            openSite(mainURL)
+                        }
+                        .frame(minHeight: 32)
+                        
+                        
+                        HStack(alignment: .center) {
+                            Text("Onboarding Again")
+                                .fontWeight(.heavy)
+                                .padding()
+                                .onTapGesture {
+                                    restartOnboard()
+                                }
+                            
+                            Spacer()
+                            Image(systemName:"chevron.compact.right").padding().foregroundColor(.white).scaleEffect(1.6)
+                        }
+                        .frame(minHeight: 32)
                     }
-                    .frame(maxHeight: 300)
-//                    .background(.gray)
-
+                    .background(Color.init(white: 1 , opacity: 0.3))
+                    .cornerRadius(5)
+                    .frame(maxHeight: 180)
+                    .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+                    
+                    Spacer()
+                   
+                } //Main VStack
+                .frame(height:UIScreen.main.bounds.height)
+//                Spacer()
                 
-            }.background(Color.init(red: 0.5, green: 0.5, blue: 1.0))
+                
+//                .frame(maxHeight: UIScreen.main.bounds.height)
+            .background(Color.init(red: 0.5, green: 0.5, blue: 1.0))
+            } //ScrollView
+//            .frame(height: UIScreen.main.bounds.height)
 //            .edgesIgnoringSafeArea(.all)
-
-        }
+            
+        }//NavigationView
+        .edgesIgnoringSafeArea(.all)
         .navigationTitle("Home")
         .navigationBarTitleDisplayMode(.large)
     }
+    
+    func openSite(_ url: URL) {
+        if UIApplication.shared.canOpenURL(url) { UIApplication.shared.open(url)
+        }
+    }
+    
+    func restartOnboard() {
+        showOnboarding = true
+    }
+    
 }
 
 struct MainPage_Previews: PreviewProvider {
--- Big Robot Micro Client Services/MainContentViews/WeatherMockView.swift
+++ Big Robot Micro Client Services/MainContentViews/WeatherMockView.swift
@@ -20,42 +20,88 @@ struct WeatherMockView: View {
     
     var body: some View {
         VStack {
-            Text("Fake Weekly Casts")
+            Text("Weather")
+                .foregroundColor(.white)
                 .font(.title)
-                .padding()
+                .fontWeight(.bold)
+                .padding(10)
+                .background(Color.init(red: 0, green: 0.5, blue: 0, opacity: 0.2))
+                .cornerRadius(3)
             Spacer()
             ScrollView(.horizontal) {
                 //                HStack {
-                //                    Spacer()
-                HStack(alignment: .bottom, spacing: 10) {
-                    
-                    if(temperatures.count >= 5) {
-                        ForEach(0..<5, content: { i in
-                            let temp = temperatures[i]
-                            VStack {
-                                tempImages.randomElement()
-                                Text("\(temp)")
-                                Spacer()
-                                Rectangle().frame(width:40,height: CGFloat(temp) * 2, alignment: .bottom)
+                Spacer()
+                Divider()
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(22)
+                        .padding()
+                        .foregroundColor(Color.init(red: 0, green: 0, blue: 0, opacity: 0.13))
+                        .frame(maxHeight: 530)
+                    VStack {
+                        Text("Sample Chart")
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                            .padding(8)
+                            .frame(width: 215)
+                            .background(Color.init(red: 0.2, green: 0.5, blue: 0.5))
+                            .cornerRadius(10)
+                        HStack(alignment: .bottom, spacing: 10) {
+                            
+                            if(temperatures.count >= 5) {
+                                ForEach(0..<5, content: { i in
+                                    let temp = temperatures[i]
+                                    VStack {
+                                        tempImages.randomElement()?.resizable()
+                                            .aspectRatio(contentMode: ContentMode.fit)
+                                            .frame(maxHeight: 25)
+                                            
+    //                                        .padding(EdgeInsets(top: 9, leading: 0, bottom: 9, trailing: 0))
+                                        Text("\(temp)")
+                                        Spacer()
+                                        Rectangle().frame(width:40,height: CGFloat(temp) * 2.4, alignment: .bottom)
+                                            .cornerRadius(4)
+                                    }
+                                    .frame(height: 330)
+                                })
                             }
-                            .frame(maxHeight: 255)
-                        })
+                        }
+                        .background(Color.init(red: 0.8, green: 0.8, blue: 0.8))
+                        .frame(width: UIScreen.main.bounds.width ,alignment: .center)
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 30, trailing: 0))
                     }
+                    .shadow(radius: 3, x:5, y:5)
                 }
-                .background(.green)
-                .frame(width: UIScreen.main.bounds.width ,alignment: .center)
-                .padding(EdgeInsets(top: 10, leading: 0, bottom: 30, trailing: 0))
-                //                    Spacer()
+                Divider()
+                Spacer()
+                Text("New Temps")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(10)
+                    .background(Color.init(red: 0, green: 0.5, blue: 0, opacity: 0.2))
+                    .cornerRadius(3)
+                    .onTapGesture {
+                        nextWeather()
+                    }
+                Spacer()
             }
             //            }
             .background(.clear)
             
         }
+        .background(Color.init(red: 0.4, green: 0.8, blue: 0.9))
         .onAppear(perform: {
             temperatures = (0..<tempCount).map { _ in
                 Int.random(in: 1...99)
             }
         })
+    }
+    
+    func nextWeather() {
+        temperatures = (0..<tempCount).map { _ in
+            Int.random(in: 1...99)
+        }
     }
     
 }
